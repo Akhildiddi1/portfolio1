e07d1015a80b973475165bd3d5567361bc69a803
Akhildiddi1 <139470251+Akhildiddi1@users.noreply.github.com>
2023-09-03 14:41:58 +0530

    Update style.js

--- style.js
+++ style.js
@@ -34,13 +34,7 @@ $(document).ready(function(){
     });
 
     // typing text animation script
-    var typed = new Typed(".typing",{
-		strings: ["Aspiring Developer ", "Coding Enthusiast", ],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-
+   
 
     // owl carousel script
     $('.carousel').owlCarousel({
